bd5c3b94bee565e11649a8f66b097a42bdb5cc46
AKHIL JP <akhilakku008@gmail.com>
2023-04-16 13:18:29 +0530

    firstCommit

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1719333.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 129400168, 129400184, 129400187, 129400148, 129400133, 129400188, 129400189, 129400190, 129400192, 129400135, 129400193, 129400137, 129400138, 129400154 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/129400178/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/129400195/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/129400176/1/ammo.js', 'preload' : true},
+];
